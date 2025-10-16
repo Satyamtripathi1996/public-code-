27f9577f6b02b87b32a436a7af9fdb11c98eb635
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-16 20:47:37 +0530

    Add descriptions to RDS variables

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,5 +1,9 @@
 variable "private_subnet_ids" {
-  type = list(string)
+  description = "List of private subnet IDs for RDS"
+  type        = list(string)
 }
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  description = "VPC ID where RDS will be deployed"
+  type        = string
+}
