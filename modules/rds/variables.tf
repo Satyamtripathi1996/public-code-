4121ab737fcf3f58b6a920d4dd6095a0c52fa072
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 20:08:21 +0530

    Refactor RDS variables in variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,9 +1,9 @@
-variable "private_subnet_ids" {
-  description = "List of private subnet IDs for RDS"
-  type        = list(string)
-}
-
-variable "vpc_id" {
-  description = "VPC ID where RDS will be deployed"
-  type        = string
-}
+variable "project"            { type = string }
+variable "vpc_id"             { type = string }
+variable "private_subnet_ids" { type = list(string) }
+variable "db_name"            { type = string }
+variable "db_username"        { type = string }
+variable "db_password"        { type = string, sensitive = true }
+variable "kms_key_id"         { type = string, default = null }
+variable "web_sg_id"          { type = string } # only web tier can reach DB
+variable "tags"               { type = map(string) }
