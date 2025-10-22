2c2ef16e5fcc76e419a3e9037e3b5e5d5c42e404
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 19:35:31 +0530

    Update provider configuration in provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,17 @@
 terraform {
+  required_version = ">= 1.6.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
-
-  required_version = ">= 1.6.0"
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
