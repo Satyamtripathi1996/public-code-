9ca501d33b750ecc3cbe9120ecedacbdd8f079d2
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 21:39:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,9 @@
-output "alb_dns_name"  { value = module.alb.alb_dns_name }
-output "rds_endpoint"  { value = module.rds.db_endpoint }
+output "alb_dns_name" {
+  description = "Load Balancer DNS"
+  value       = module.alb.alb_dns_name
+}
+
+output "rds_endpoint" {
+  description = "RDS PostgreSQL endpoint"
+  value       = module.rds.db_endpoint
+}
