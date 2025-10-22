9251f85ef37b0d646fe0a88fb433e91036644bf7
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 21:32:28 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,1 +1,8 @@
-output "db_endpoint" { value = aws_db_instance.this.address }
+variable "project"            { type = string }
+variable "vpc_id"             { type = string }
+variable "private_subnet_ids" { type = list(string) }
+variable "db_name"            { type = string }
+variable "db_username"        { type = string }
+variable "db_password"        { type = string, sensitive = true }
+variable "web_sg_id"          { type = string }
+variable "tags"               { type = map(string) }
