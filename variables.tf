ccaabee11641a0bc734345c870f3e34a904924ba
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 21:39:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,19 @@
-variable "region"        { type = string, default = "us-east-1" }
-variable "project_name"  { type = string, default = "north-eval-eda" }
-variable "vpc_cidr"      { type = string, default = "10.20.0.0/16" }
-variable "azs"           { type = list(string), default = ["us-east-1a","us-east-1b"] }
+variable "region"       { type = string, default = "us-east-1" }
+variable "project_name" { type = string, default = "north-eval-eda" }
 
-# who can hit the ALB (use your /32 or corp CIDR ranges)
+variable "vpc_cidr" { type = string, default = "10.20.0.0/16" }
+variable "azs"      { type = list(string), default = ["us-east-1a","us-east-1b"] }
+
+# Who can hit the public ALB (your /32 or corporate ranges)
 variable "allowed_ingress_cidrs" { type = list(string) }
 
-# ACM for HTTPS (Issued in us-east-1)
-variable "acm_certificate_arn"   { type = string }
+# ACM certificate (Issued in us-east-1) for HTTPS
+variable "acm_certificate_arn" { type = string }
 
+# EC2
 variable "instance_type" { type = string, default = "t3.micro" }
 
-# DB settings (PostgreSQL)
+# PostgreSQL settings
 variable "db_name"     { type = string, default = "appdb" }
 variable "db_username" { type = string, default = "dbadmin" }
 variable "db_password" { type = string, sensitive = true }
