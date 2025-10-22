8251d278e6ec3ffc2354bcfa594ce05757ec8279
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 19:39:40 +0530

    Refactor variables in vpc module

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,4 @@
-variable "vpc_cidr" {
-  type = string
-}
+variable "project"   { type = string }
+variable "vpc_cidr"  { type = string }
+variable "azs"       { type = list(string) }
+variable "tags"      { type = map(string) }
