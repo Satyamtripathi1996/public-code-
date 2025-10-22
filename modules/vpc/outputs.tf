9c00f44c8dbb5d194d1d046e3a272dfa2d56d2c5
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 19:46:17 +0530

    Refactor VPC outputs to use dynamic subnet lists

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = aws_vpc.this.id
 }
 
 output "public_subnet_ids" {
-  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  value = [for k, s in aws_subnet.public  : s.id]
 }
 
 output "private_subnet_ids" {
-  value = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+  value = [for k, s in aws_subnet.private : s.id]
 }
