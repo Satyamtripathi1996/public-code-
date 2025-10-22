3c100011b15d040119d7c0d062218b0fb7e6a9bc
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 22:51:36 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -44,15 +44,18 @@ resource "aws_lb_target_group" "web" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  # Relaxed matcher during bootstrap; can tighten later to 200-399
   health_check {
     path                = "/"
     protocol            = "HTTP"
-    matcher             = "200-399"
+    matcher             = "200-499"
     healthy_threshold   = 2
     unhealthy_threshold = 2
     interval            = 30
     timeout             = 5
   }
+
   tags = merge(var.tags, { Name = "${local.name}-tg" })
 }
 
@@ -71,7 +74,7 @@ resource "aws_lb_listener" "http_redirect" {
   }
 }
 
-# HTTPS listener
+# HTTPS listener (ACM)
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.this.arn
   port              = 443
