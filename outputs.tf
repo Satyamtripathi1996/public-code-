01d97e304c92ea525c13b4858f3e9a3fd4517d98
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 21:19:30 +0530

    Simplify outputs in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,2 @@
-output "alb_dns_name" {
-  description = "Public URL for the app"
-  value       = module.alb.alb_dns_name
-}
-
-output "rds_endpoint" {
-  description = "RDS PostgreSQL writer endpoint"
-  value       = module.rds.db_endpoint
-}
+output "alb_dns_name"  { value = module.alb.alb_dns_name }
+output "rds_endpoint"  { value = module.rds.db_endpoint }
