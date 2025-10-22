26525be0eb74900d158d46f5084c9d95b51c3067
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-22 21:42:01 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,8 +1,7 @@
-variable "project"              { type = string }
-variable "vpc_id"               { type = string }
-variable "private_subnet_ids"   { type = list(string) }
-variable "instance_type"        { type = string }
-variable "target_group_arn"     { type = string }
-variable "alb_security_group_id"{ type = string }
-variable "kms_key_id"           { type = string, default = null }
-variable "tags"                 { type = map(string) }
+variable "project"               { type = string }
+variable "vpc_id"                { type = string }
+variable "private_subnet_ids"    { type = list(string) }
+variable "instance_type"         { type = string }
+variable "target_group_arn"      { type = string }
+variable "alb_security_group_id" { type = string }
+variable "tags"                  { type = map(string) }
