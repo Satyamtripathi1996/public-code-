3b6862f316e95bc84cf8bd606ecc8c81c4b3fe8a
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-23 17:10:56 +0530

    Update main.tf

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,19 +1,19 @@
-locals { name = "${var.project}-bastion" }
+locals { name = var.project }
 
-resource "aws_security_group" "this" {
-  name   = "${locals.name}-sg"
-  vpc_id = var.vpc_id
+resource "aws_security_group" "bastion" {
+  name        = "${local.name}-bastion-sg"
+  description = "SSH from allowed CIDRs to bastion"
+  vpc_id      = var.vpc_id
 
-  # SSH into bastion only from your /32 (or corp ranges)
   ingress {
-    description = "SSH from your IP /32"
+    description = "SSH from your IPs"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.allowed_cidrs_ssh
+    cidr_blocks = var.allowed_ingress_cidrs
   }
 
-  # Outbound open so bastion can yum/apt + SSH to private instances
+  # Egress can be wide or restricted; here minimal to 22/80/443 for admin use
   egress {
     from_port   = 0
     to_port     = 0
@@ -21,22 +21,22 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = merge(var.tags, { Name = "${locals.name}-sg" })
+  tags = merge(var.tags, { Name = "${local.name}-bastion-sg" })
+}
+
+resource "aws_instance" "bastion" {
+  ami                    = data.aws_ami.al2.id
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+  key_name               = var.key_name
+  associate_public_ip_address = true
+
+  tags = merge(var.tags, { Name = "${local.name}-bastion" })
 }
 
 data "aws_ami" "al2" {
   most_recent = true
   owners      = ["amazon"]
   filter { name = "name" values = ["amzn2-ami-hvm-*-x86_64-gp2"] }
-}
-
-resource "aws_instance" "this" {
-  ami                         = data.aws_ami.al2.id
-  instance_type               = var.instance_type
-  subnet_id                   = var.public_subnet_id
-  associate_public_ip_address = true
-  key_name                    = var.key_name
-  vpc_security_group_ids      = [aws_security_group.this.id]
-
-  tags = merge(var.tags, { Name = locals.name })
 }
