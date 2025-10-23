1a52f6d1e0722bfa3e51cc8afc0435ad6cc38ddb
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-23 17:09:20 +0530

    Update variables.tf

--- modules/bastion/variables.tf
+++ modules/bastion/variables.tf
@@ -1,7 +1,31 @@
-variable "project"           { type = string }
-variable "vpc_id"            { type = string }
-variable "public_subnet_id"  { type = string }       # one public subnet id
-variable "allowed_cidrs_ssh" { type = list(string) } # e.g. ["165.1.207.240/32"]
-variable "instance_type"     { type = string, default = "t3.micro" }
-variable "key_name"          { type = string }       # existing key pair name
-variable "tags"              { type = map(string) }
+variable "project" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  description = "Public subnets for bastion (pick one index in module)."
+  type        = list(string)
+}
+
+variable "allowed_ingress_cidrs" {
+  description = "CIDRs allowed to SSH into bastion (e.g., [\"165.1.207.240/32\"])."
+  type        = list(string)
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing EC2 key pair name to SSH into bastion."
+  type        = string
+}
+
+variable "tags" {
+  type = map(string)
+}
