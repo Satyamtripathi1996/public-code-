ee913480b010e1f0bbeabb6307f17e3df85f6a70
SATYAM TRIPATHI <56503829+Satyamtripathi1996@users.noreply.github.com>
2025-10-23 14:24:58 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,7 +1,14 @@
-variable "project"               { type = string }
-variable "vpc_id"                { type = string }
-variable "private_subnet_ids"    { type = list(string) }
-variable "instance_type"         { type = string }
-variable "target_group_arn"      { type = string }
+variable "project"            { type = string }
+variable "vpc_id"             { type = string }
+variable "private_subnet_ids" { type = list(string) }
+variable "instance_type"      { type = string }
+variable "target_group_arn"   { type = string }
 variable "alb_security_group_id" { type = string }
-variable "tags"                  { type = map(string) }
+variable "tags"               { type = map(string) }
+
+# NEW — to allow SSH only from bastion
+variable "bastion_sg_id" {
+  description = "Security group ID of bastion allowed to SSH into web EC2"
+  type        = string
+  default     = null
+}
